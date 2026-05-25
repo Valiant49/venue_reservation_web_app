0635e4702916601a566bdb4f8a43a7544b2a9ee5
Valiant49 <vichandreicortez49@gmail.com>
2026-05-25 16:58:36 +0800

    transposed table for Facility delete view

--- resources/views/facility/delete.blade.php
+++ resources/views/facility/delete.blade.php
@@ -1,31 +1,39 @@
 @isset($facility)
-<div>
-    <h3>Are you sure you want to delete this facility?</h3>
-    <table>
-        <tr>
-            <th>Facility Code</th>
-            <th>Name</th>
-            <th>Type</th>
-            <th>Base Fee</th>
-            <th>Capacity</th>
-            <th>Description</th>
-        </tr>
-        <tr>
-            <td>{{ $facility->facility_code }}</td>
-            <td>{{ $facility->facility_name }}</td>
-            <td>{{ $facility->facility_type }}</td>
-            <td>{{ $facility->base_fee }}</td>
-            <td>{{ $facility->capacity }}</td>
-            <td>{{ $facility->description }}</td>
-        </tr>
-    </table>
+    <div>
+        <h3>Are you sure you want to delete this facility?</h3>
+        <table>
+            <tr>
+                <th>Facility Code</th>
+                <td>{{ $facility->facility_code }}</td>
+            </tr>
+            <tr>
+                <th>Name</th>
+                <td>{{ $facility->facility_name }}</td>
+            </tr>
+            <tr>
+                <th>Type</th>
+                <td>{{ $facility->facility_type }}</td>
+            </tr>
+            <tr>
+                <th>Base Fee</th>
+                <td>{{ $facility->base_fee }}</td>
+            </tr>
+            <tr>
+                <th>Capacity</th>
+                <td>{{ $facility->capacity }}</td>
+            </tr>
+            <tr>
+                <th>Description</th>
+                <td>{{ $facility->description }}</td>
+            </tr>
+        </table>
 
-    <form action="/facility/{{ $facility->facility_id }}" method="POST">
-        @csrf
-        @method('DELETE')
-        <button type="submit">Yes, Delete</button>
-    </form>
+        <form action="/facility/{{ $facility->facility_id }}" method="POST">
+            @csrf
+            @method('DELETE')
+            <button type="submit">Yes, Delete</button>
+        </form>
 
-    <a href="/facility">Cancel</a>
-</div>
+        <a href="/facility">Cancel</a>
+    </div>
 @endisset
