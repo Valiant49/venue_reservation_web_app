533f2e1a62196ae5382322832e9f39efc43d0476
Valiant49 <vichandreicortez49@gmail.com>
2026-05-29 23:30:44 +0800

    major UI update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,20 +15,20 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
+        <div class="min-h-screen bg-surface">
             @include('layouts.navigation')
 
             <!-- Page Heading -->
-            @isset($header)
-                <header class="bg-white dark:bg-gray-800 shadow">
+            {{-- @isset($header)
+                <header class="bg-primary  shadow">
                     <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
                         {{ $header }}
                     </div>
                 </header>
-            @endisset
+            @endisset --}}
 
             <!-- Page Content -->
-            <main>
+            <main class="max-w-7xl mx-auto px-4">
                 {{ $slot }}
             </main>
         </div>
